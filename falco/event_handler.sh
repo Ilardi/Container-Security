ee80731155f6dd611be0b8485988db7123306c10
Ilardi <luci.ilardi@studenti.unina.it>
2024-12-22 05:16:13 -0500

    added falco folder

---
+++ falco/event_handler.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+output_file="/var/log/openport.txt"
+
+check_open_port() {
+  echo "$event" >> "$output_file"
+}
+
+dispatch_event() {
+  event="$1"
+  
+  # Extract the "tags" field and check for "open_port"
+  tags=$(echo "$event" | jq -r '.tags')
+
+  if [[ "$tags" == *"open_port"* ]]; then
+    check_open_port "$event"
+  fi
+}
+
+# Read events from stdin (passed by Falco)
+while IFS= read -r line; do
+  dispatch_event "$line"
+done
